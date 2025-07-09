06bb1868838e70b2d2c6c3c015fa1d1ca7c8b274
Pascal Martin <connect@pascalmartin.dev>
2025-07-09 22:28:00 +1000

    Customer model created with constructor

--- lsk-webex-hpw/src/models/customer.ts
+++ lsk-webex-hpw/src/models/customer.ts
@@ -14,4 +14,18 @@ export class Customer{
   lastOrderItems?: string;
   memberPromotion?: string;
 
+  // RESTART HERE!! Constructure may or may not be setup correctly!!
+  constructor(data: Consumer | null) {
+	this.lastname = data?.lastname || "";
+	this.city = data?.city || "";
+	this.firstname = data?.firstname || "";
+	this.addressLine1 = data?.addressLine1 || "";
+	this.email = data?.email || "";
+	this.identifier = data?.identifier || "";
+	this.phoneNumber1 = data?.phoneNumber1 || "";
+	this.consumer = data?.consumer || "";
+	this.zipCode = data?.zipCode || "";
+	this.addressLine2 = data?.addressLine2 || "";
+  }
+
 }
