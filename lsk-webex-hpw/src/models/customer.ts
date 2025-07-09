edddc1905fba233ffc6b9db169b26556cab71cef
Pascal Martin <connect@pascalmartin.dev>
2025-07-09 22:24:35 +1000

    First part of customer model created

---
+++ lsk-webex-hpw/src/models/customer.ts
@@ -0,0 +1,17 @@
+export class Customer{
+  firstname?: string;
+  lastname?:string;
+  email?: string;
+  phone?: string
+  group?: string;
+  street?: string;
+  city?: string;
+  state?: string;
+  postcode?: string;
+  isVipMember?: string;
+  posLastVisitDate?: string;
+  wineClubFirstJoinedDate?: string;
+  lastOrderItems?: string;
+  memberPromotion?: string;
+
+}
