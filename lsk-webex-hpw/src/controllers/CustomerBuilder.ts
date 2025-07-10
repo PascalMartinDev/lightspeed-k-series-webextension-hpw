2ced2373a98a2d250f5dd417a85ae3ad01992de9
Pascal Martin <connect@pascalmartin.dev>
2025-07-10 16:15:20 +1000

    Updated Customer model and starting framework of CustomerBuilder class - Builder Pattern

---
+++ lsk-webex-hpw/src/controllers/CustomerBuilder.ts
@@ -0,0 +1,11 @@
+
+
+export class CustomerBuilder {
+
+}
+
+
+
+/* 
+Builder Pattern - Customer Builder class facilites the building of the Customer model from various different data sources. 
+*/
--- lsk-webex-hpw/src/models/customer.ts
+++ lsk-webex-hpw/src/models/customer.ts
@@ -1,31 +1,18 @@
-export class Customer{
-  firstname?: string;
-  lastname?:string;
-  email?: string;
-  phone?: string
-  group?: string;
-  street?: string;
-  city?: string;
-  state?: string;
-  postcode?: string;
-  isVipMember?: string;
-  posLastVisitDate?: string;
-  wineClubFirstJoinedDate?: string;
-  lastOrderItems?: string;
-  memberPromotion?: string;
+// Customer model
 
-  // RESTART HERE!! Constructure may or may not be setup correctly!!
-  constructor(data: Consumer | null) {
-	this.lastname = data?.lastname || "";
-	this.city = data?.city || "";
-	this.firstname = data?.firstname || "";
-	this.addressLine1 = data?.addressLine1 || "";
-	this.email = data?.email || "";
-	this.identifier = data?.identifier || "";
-	this.phoneNumber1 = data?.phoneNumber1 || "";
-	this.consumer = data?.consumer || "";
-	this.zipCode = data?.zipCode || "";
-	this.addressLine2 = data?.addressLine2 || "";
-  }
-
+export class Customer {
+  firstname: string = "";
+  lastname: string = "";
+  email: string = "";
+  phone: string = "";
+  group: string = "";
+  street: string = "";
+  city: string = "";
+  state: string = "";
+  postcode: string = "";
+  isVipMember: string = "";
+  posLastVisitDate: string = "";
+  wineClubFirstJoinedDate: string = "";
+  lastOrderItems: string = "";
+  memberPromotion: string = "";
 }
