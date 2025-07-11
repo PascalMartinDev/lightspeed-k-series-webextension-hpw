128cfc7f81670a02fc94b451ae06d2d3e2a2c073
Pascal Martin <connect@pascalmartin.dev>
2025-07-11 14:13:52 +1000

    Setup env variable for credentals connecting to AWS

--- lsk-webex-hpw/src/main.ts
+++ lsk-webex-hpw/src/main.ts
@@ -1,8 +1,13 @@
-// Dom Content Loaded Script to initialise 
-document.addEventListener("DOMContentLoaded", () => {
-  const accountElement = document.getElementById("account");
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const TEST_ENV: string = process.env.AWS_REGION || 'default-region';
+// Dom Content Loaded Script to initialise
+document.addEventListener('DOMContentLoaded', () => {
+  const accountElement = document.getElementById('account');
   if (accountElement) {
-    accountElement.innerHTML = "TEST: Function Page Connected";
+    accountElement.innerHTML = TEST_ENV;
   } else {
     console.error("Element with ID 'account' not found");
   }
--- lsk-webex-hpw/src/services/ContactDB.ts
+++ lsk-webex-hpw/src/services/ContactDB.ts
@@ -1,1 +1,22 @@
+//import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+//import { DynamoDBDocumentClient, GetCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
 
+// Interface for the expectant contact item structure:
+
+interface ContactRecord {
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  groups: string;
+  street: string;
+  city: string;
+  customer_state: string;
+  postcode: string;
+  last_order_items: string;
+  pos_member_notes: string;
+  wine_club_first_joined_date: string;
+}
+
+// ContactDB class to handle earching and fetching contact record:
+export class ContactDB {}
